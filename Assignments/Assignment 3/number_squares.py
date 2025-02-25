6ab3147718afb56cec05026386aaaf27a6d66e76
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2025-02-25 17:16:10 +0530

    add number_squares.py

---
+++ Assignments/Assignment 3/number_squares.py
@@ -0,0 +1,6 @@
+# Write a Python program that uses a for loop to iterate through a list of numbers and prints each number and its square. 
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for num in numbers:
+    print(f"{num}\t{num ** 2}")
